efb5435eeb6b74f172fc3c989b57bf0610c97a92
mukzishan <mukzishan.13@gmail.com>
2025-04-06 22:42:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="id">
+<meta charset='UTF-8'/><meta content='width=device-width, initial-scale=1, user-scalable=1, minimum-scale=1, maximum-scale=5' name='viewport'/><meta content='IE=edge' http-equiv='X-UA-Compatible'/>
+
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Sono:wght@600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
+
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.0.19/dist/sweetalert2.all.min.js"></script>
+  <script src="https://unpkg.com/typeit@8.7.0/dist/index.umd.js"></script><link href="https://feeldreams.github.io/dibacayuk/style.css" rel="stylesheet" type="text/css" />
+  <script src="https://kit.fontawesome.com/4f3ce16e3e.js" crossorigin="anonymous"></script>
+
+<head>
+<title>Makasih Ayang</title>
+<link rel="icon" type="image/x-icon" href="https://malasid.github.io/favicon.png">
+<meta name="description" content="HTML Bucin Malas.id">
+<!-- 
+  Made with love by Ryan!
+
+  Thanks to all <3
+-->
+</head>
+<body>
+
+   <!-- Ganti Audio di sini -->
+   <audio src="https://feeldreams.github.io/audio/nightchanges.mp3" id="linkmp3" class="sembunyi"></audio>
+
+   <div id="bodyblur">
+     <!-- Wallpaper / Background --><img src="https://feeldreams.github.io/pics/awan.jpg" id="wallpaper"/>
+   </div>
+
+   <div id='Content'>
+
+     <div id="ftAwal">
+       <!-- Stiker Pembuka -->
+       <img src="https://feeldreams.github.io/pandaputih.gif" id="ftoAwal"/>
+     </div>
+
+     <div id="loveIn">
+       <!-- Tombol LOVE --><label class='lovein'>&#10084;</label>
+     </div>
+     <p id="ket">Sentuh LOVEnya!</p>
+
+     <div class="kumpulanstiker">
+         <!-- Stiker untuk Konten -->
+         <img src="https://feeldreams.github.io/pusn.gif" id="fotostiker"/>
+         <img src="https://feeldreams.github.io/wortel.gif" id="fotostiker1"/>
+         <img src="https://feeldreams.github.io/ngumpet.gif" id="fotostiker2"/>
+         <img src="https://feeldreams.github.io/bwa2.gif" id="fotostiker3"/>
+     </div>
+
+     <div><div id='pergeseran'>
+
+        <!-- Edit Pesan di Sini -->
+        <p><b>
+          <span>hii ayaangg aku Jihan ❤ <br>makasii yaa</span>
+        </b></p>
+
+        <p><b>
+          <span>makasi buat apa hayoo?</span>
+        </b></p>
+
+        <p><b>
+          <span>baca sampai akhir yaa 😋</span>
+        </b></p>
+
+     </div></div>
+
+     <p id="ketgeser">Klik untuk Geser!</p>
+
+     <!-- Edit Pesan Akhir di Sini -->
+     <div><blockquote id='bq'>
+       <p id="kalimat">aku cuma mau bilang terimakasih udah selalu bersamaku dan mau nerima aku apa adanya..  dan asal kamu tau gimana beruntungnya aku semenjak deket sama kamu,  dengan kamu yang sederhana tapi kamu selalu punya cara buat aku <i>happy</i> bahkan dari hal kecil yang belum pernah aku dapatkan sebelumnya..  terimakasih banyak yaa, dari kamu aku merasa dihargai.. seberuntung itu aku memiliki kamu, kamu rumah aku, kamu penenang aku kalo aku ada masalah,
+        Terimakasiiii kasangkuuu sudahhh menjadi yang terbaik untuk akuu, aku sangat berterima kasih atas semuanya,aku sayang sama kamu untuk selamanyaa, maaf mungkin aku masihh banyak kurangnya dan aku permintaannya terlalu banyak karena aku nyaman sama kamu,aku selalu minta maaf, karna aku gamau kehilangan kamu ❤
+        Makasii yaa udah nerima aku apa adanya, semoga kamuu selaluu bisa memaklumi kekuranganku,terimaakasiii udah sayang dan tulus sama akuu, tolong jangan pergi yaa? jangan pernah duain akuu dan aku percaya sama kamuu, tolong jaga kepercaan aku untuk selamanya sampai kita menjadi ikatan Halal nanti 🤗🥹 </p>
+       <p id="pesanAkhir">i love uuu Mooreee >< 🤍🥰🥰😍😘</p>
+     </blockquote></div>
+
+   </div>
+
+<script>
+  const body = document.querySelector("body"); const iniwp = [];iden = 1; const swalst = Swal.mixin({timer: 2500, allowOutsideClick: false, showConfirmButton: false, timerProgressBar: true, imageHeight: 90,}); audio = new Audio('' + linkmp3.src); ftganti=0;fungsi=0;fungsiAwal=0;deffotostiker=fotostiker.src;function berjatuhan() {const heart = document.createElement("div"); heart.className = "fas fa-heart"; heart.style.left = (Math.random() * 90)+"vw"; heart.style.animationDuration = (Math.random()*3)+2+"s"; body.appendChild(heart);} setInterval(function name(params) {var heartArr = document.querySelectorAll(".fa-heart"); if (heartArr.length > 100) {heartArr[0].remove()}},100);Content.style = "opacity:1;margin-top:14vh"; const swals = Swal.mixin({allowOutsideClick: false, cancelButtonColor: '#FF0040', imageHeight: 80,}); 
+</script>
+<script src="https://malasid.github.io/html/makasiayang.js"></script>
+<!-- Sampai Sini -->
+</body>
+</html>
